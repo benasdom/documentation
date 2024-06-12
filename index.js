600359d96fb5c112a0149bd5821d504dd55ce245
benasdom <benasdomd@gmail.com>
2024-06-12 23:47:23 +0000

    menu

--- index.js
+++ index.js
@@ -20,3 +20,17 @@ document.body.onload=()=>{
     document.querySelector(".rmaincontent").style.animationName="slid";
 },100);
 }
+
+function hideOptions(){
+
+    [...document.querySelectorAll(".leftmain")].map(a=>a.classList.toggle("a"));
+    [...document.querySelectorAll(".right")].map(a=>a.classList.toggle("b"));
+    [...document.querySelectorAll(".lmaintop")].map(a=>a.classList.toggle("c"));
+    [...document.querySelectorAll(".subtopa")][1].classList.toggle("d");
+    [...document.querySelectorAll(".lmaintop")].map(a=>a.children[1].classList.toggle("e"));
+
+
+    
+}
+document.querySelector(".hider")
+.addEventListener('click',hideOptions)
